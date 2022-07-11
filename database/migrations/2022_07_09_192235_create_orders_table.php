69be1ad1bf33081f54337e96fede59e8aba00988
Maxim Dacenko <shadrmax@ukr.net>
2022-07-11 16:01:26 +0300

    first commit

---
+++ database/migrations/2022_07_09_192235_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->mediumInteger('user_id');
+            $table->string('service_type');
+            $table->text('description')->nullable();
+            $table->string('inn')->nullable();
+            $table->string('lfp')->nullable();
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
